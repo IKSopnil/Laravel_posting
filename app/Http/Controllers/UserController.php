884f77df850e7321a98bfdc18e304a06efc4bf70
IKSopnil <sopnil4o4o@gmail.com>
2023-12-29 18:16:55 +0600

    m

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+
+    public function logout(){
+
+        auth()->logout();
+        return redirect('/');
+    }
     public function register(Request $request)
 {
     $incoming_field=$request->validate([
-        'name'=>['required','min:3','max:10'],
-        'email'=>['required','email'],
-        'password'=>['required','min:8','max:200']
+        'name'=>['required','min:3','max:20',Rule::unique('users','name')],
+        'email'=>['required','email',Rule::unique('users','email')],
+        'password'=>['required','min:3','max:200']
     ]);
 
     $incoming_field['password']= bcrypt($incoming_field['password']);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <div style="border: 3px solid black;">
-    <h2>Resgister</h2>
-    <form action="/register" method="POST">
-        @csrf
-        <input name="name" type="text" placeholder="Name">
-        <input name="email" type="text" placeholder="Email">
-        <input name="password" type="password" placeholder="Password">
-        <button>Register</button>
-    </form>
-    </div>
+
+    @auth
+
+        <p>ur logged in</p>
+        <form action="/logout" method="POST">
+            @csrf
+            <button>logout</button>
+        </form>
+    @else
+        <div style="border: 3px solid black;">
+            <h2>Resgister</h2>
+            <form action="/register" method="POST">
+                @csrf
+                <input name="name" type="text" placeholder="Name">
+                <input name="email" type="text" placeholder="Email">
+                <input name="password" type="password" placeholder="Password">
+                <button>Register</button>
+            </form>
+        </div>
+
+    @endauth
+
 </body>
+
 </html>
