bec094f732322bb05e9731a40f83d58f59a49877
IKSopnil <sopnil4o4o@gmail.com>
2023-12-30 18:26:43 +0600

    m

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,25 +9,37 @@ use Illuminate\Validation\Rule;
 class UserController extends Controller
 {
 
-    public function login(){
+    public function login(Request $request)
+    {
+        $incoming_field = $request->validate([
+            "loginname" => 'required',
+            "loginpassword" => 'required',
 
+        ]);
+
+        if (auth()->attempt(['name' => $incoming_field['loginname'], 'password' => $incoming_field['loginpassword']])) {
+            $request->session()->regenerate();
+        }
+        return redirect('/');
     }
 
-    public function logout(){
+    public function logout()
+    {
 
         auth()->logout();
         return redirect('/');
     }
     public function register(Request $request)
     {
-    $incoming_field=$request->validate([
-        'name'=>['required','min:3','max:20',Rule::unique('users','name')],
-        'email'=>['required','email',Rule::unique('users','email')],
-        'password'=>['required','min:3','max:200']
-    ]);
+        $incoming_field = $request->validate([
+            'name' => ['required', 'min:3', 'max:20', Rule::unique('users', 'name')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:3', 'max:200']
+        ]);
 
-    $incoming_field['password']= bcrypt($incoming_field['password']);
-    $user=User::create($incoming_field);
-    auth()->login($user);
-    return redirect('/');
-}}
+        $incoming_field['password'] = bcrypt($incoming_field['password']);
+        $user = User::create($incoming_field);
+        auth()->login($user);
+        return redirect('/');
+    }
+}
