cd3e72aa8ed45bbc4b65fd14d0daa16dee32802d
IKSopnil <sopnil4o4o@gmail.com>
2024-02-11 10:47:27 +0600

    m

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -119,13 +119,13 @@
                 <h2>Login</h2>
                 <form action="/login" method="POST">
                     @csrf
-                    <input name="loginname" type="text" placeholder="Name">
-                    <input name="loginpassword" type="password" placeholder="Password">
+                    <input name="loginname" type="text" placeholder="Name"> <br>
+                    <input name="loginpassword" type="password" placeholder="Password"><br>
                     <button>Login</button>
                 </form>
             </div>
         </div>
-        
+
         <div class="form-container">
             <div style="border: 3px solid black;">
                 <h2>Register</h2>
